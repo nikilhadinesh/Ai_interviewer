73bc8ea2fed75435fd1e2c6d61b6f8c5099cce23
nikilhadinesh <nikilhadinesh@gmail.com>
2025-10-07 16:58:31 +0530

    updated

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -84,7 +84,7 @@ const AuthForm = ({type} : {type : FormType}) => {
     <div className="card-border lg:min-w-[566px]">
       <div className="flex flex-col gap-6 card py-14 px-10">
         <div className="flex flex-row gap-2 justify-center">
-          <Image src="/logo1.svg" alt="logo" height={32} width={38} />
+          <Image src="/return.png" alt="logo" height={58} width={52} />
           <h2 className="text-primary-100">MockMate</h2>
         </div>
 
